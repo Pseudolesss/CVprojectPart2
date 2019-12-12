8f2fa55a2f86fd7b19776357eb62a61d45b6f905
Joachim <joachim.r96@hotmail.be>
2019-12-12 22:40:53 +0100

    evaluateElpsClassical

--- evaluateElpsClassical.py
+++ evaluateElpsClassical.py
@@ -1,16 +1,13 @@
 import cv2
 import csv
 import os
+import sys
 import numpy as np
 from pathlib import Path
-
-import cv2
-import csv
-import os
-import numpy as np
-from pathlib import Path
+from imgTools import display, multiDisplay
 from metrics import metric_elps
 from classical import youghongQiangEllipse
+
 
 database_directory = os.path.join(os.getcwd(), 'images_database')
 annotationFile = os.path.join(database_directory, 'CV2019_Annots.csv')
@@ -54,7 +51,7 @@ def extract_annotations_eye():
 def get_model_data_eye_ellipse():
     image_names = []
     images_list = []
-    result = list(Path("images_database/eyes/partial/").glob('*.png'))
+    result = list(Path("images_database/eyes/preprocess/").glob('*.png'))
     for file in result:  # fileName
         image = cv2.imread(str(file.resolve()), cv2.IMREAD_GRAYSCALE)
         images_list.append(image)
@@ -92,31 +89,49 @@ def detect_elps_classical(img):
 		[list of numpy array in float32] A list of the ellipse detected with for each ellipse an array : 
 		Xc, Yc (the coord. of center), theta (angle of main axis in degree), a (half length of main axis), b ((half
 		length of sub axis).
-	"""
-	# Preprocess
-	_, imageBinary = cv2.threshold(img, 10, 255, cv2.THRESH_BINARY)
-	
+	"""	
 	# Detect ellipses
-	elps = youghongQiangEllipse(imageBinary, 300, 10, 150, 10, 150, 360, 4)
+	elps = youghongQiangEllipse(imageBinary, 15, 50, 5, 25, 40, 7, 0.5)
 	return [np.array([el[0][0], el[0][1], el[2], el[1][0], el[1][1]], dtype=np.float32) for el in elps]
 
 if __name__ == '__main__':
 	images_list, annotations_list, annotations_dict = get_model_data_eye_ellipse()
 	images_list_no_elps = get_model_data_eye_no_ellipse()
 	
+	print(len(images_list))
+	print(len(annotations_list))
+	
 	m1 = 0
 	m2 = 0
-	lim = 1#len(images_list)
+	lim = len(images_list)
+	sys.stdout.write("\rProgress : 0% ")
+	sys.stdout.flush()
 	for i in range(lim):
+		sys.stdout.write(
+			"\rProgress : %.2f%% \t - Value m1 = %.2f \t m2 = %.2f    " %((i+1) / lim * 100, m1 , m2))
+		sys.stdout.flush()
 		img = images_list[i]
+		#img = cv2.resize(img,(360, 200)) # Images resized to 360p
+		img = cv2.Canny(img, 20, 100)
 		ann = [[np.array(annotations_list[i], dtype=np.float32)]]
 		det = [detect_elps_classical(img)]
 		
 		m1i, m2i = metric_elps([ann], det, [1 / 2, 1 / 2, 1 / 10, 1 / 3, 1 / 3])
-		m1 += m1i
-		m2 += m2i
-	m1 = m1/lim
-	m2 = m2/lim
+		m1 = (m1*i + m1i)/(i+1)
+		m2 = (m2*i + m2i)/(i+1)
+		
+		"""
+		img2 = img.copy()*0
+		img3 = img.copy()*0
+		for ell in det[0]:
+			cv2.ellipse(img2, (ell[0], ell[1]), (ell[3], ell[4]), ell[2], 0, 360, (255))
+		for ell in ann[0]:
+			cv2.ellipse(img3, (ell[0], ell[1]), (ell[3], ell[4]), ell[2], 0, 360, (255))
+		img4 = cv2.merge([img*0, img2, img3])
+		print(f"\n\nNumber of ellipses detected : {len(det[0])}")
+		multiDisplay(['Img provided','Ellipse detected', 'True ellipse', 'All'], [img, img2, img3, img4], 2, height=7)
+		"""
+	m1 = m1
+	m2 = m2
 	
-	print(f"\n\nThe metrics are : \nmetricElps_default = {m1}\nmetricElps_notPenalized = {m2}")
-
+	print(f"The metrics are : \nmetricElps_default = {m1}\nmetricElps_notPenalized = {m2}")
--- metrics.py
+++ metrics.py
@@ -53,40 +53,47 @@ def metric_elps(ground_truth, detected, weight=[1 / 2, 1 / 2, 1 / 5, 1 / 3, 1 / 
         l1i = len(ground_truth[i])
         l2i = len(detected[i])
         num = 0
-        
-        denom1 = max(l1i, l2i)  # denom increases if too many elps detected compared to the truth
-        denom2 = l1i # non penalizing denominator
 
-        # compute all distances
-        dists = np.zeros((l1i, l2i))
-        for j in range(l1i):
-            for k in range(l2i):
-                dists[j, k] = dist(ground_truth[i][j], detected[i][k], weight)
+        if l1i == 0:
+            if l2i == 0:
+                eval1 += 1
+                eval2 += 1
+        else:
+            denom1 = max(l1i, l2i)  # denom increases if too many elps detected compared to the truth
+            denom2 = l1i # non penalizing denominator
 
-        # find clostest to ground truth
-        for j in range(min(l1i, l2i)):
-            if j > 0:
+            # compute all distances
+            dists = np.zeros((l1i, l2i))
+            for j in range(l1i):
+                for k in range(l2i):
+                    dists[j, k] = dist(ground_truth[i][j], detected[i][k], weight)
+
+            # find clostest to ground truth
+            for j in range(min(l1i, l2i)):
+                if j > 0:
+                    if debug:
+                        print(f"Min = dists[{ind1},{ind2}] = {dists[ind1, ind2]}")
+                    # remove line of elps ground truth used and elps detected used
+                    dists = np.delete(dists, ind2, 1)
+                    dists = np.delete(dists, ind1, 0)
                 if debug:
-                    print(f"Min = dists[{ind1},{ind2}] = {dists[ind1, ind2]}")
-                # remove line of elps ground truth used and elps detected used
-                dists = np.delete(dists, ind2, 1)
-                dists = np.delete(dists, ind1, 0)
-            if debug:
-                print(dists)
-            argmin = np.argmin(dists)
-            ind2 = argmin % dists.shape[1]
-            ind1 = int((argmin - ind2) / dists.shape[1])
+                    print(dists)
+                argmin = np.argmin(dists)
+                ind2 = argmin % dists.shape[1]
+                ind1 = int((argmin - ind2) / dists.shape[1])
 
-            delta = 0.00001
-            num += min(1, (tresh_dist + delta) / (dists[ind1, ind2] + delta))
-        if debug:
-            print(f"Min = dists[{ind1},{ind2}] = {dists[ind1, ind2]}")
-        eval1 += num / denom1
-        eval2 += num / denom2
+                delta = 0.00001
+                num += min(1, (tresh_dist + delta) / (dists[ind1, ind2] + delta))
+            if debug:
+                print(f"Min = dists[{ind1},{ind2}] = {dists[ind1, ind2]}")
+            eval1 += num / denom1
+            eval2 += num / denom2
     return eval1 / l1, eval2 / l1
 
 
 if __name__ == "__main__":
-    gt = [[np.array([1, 3.5, 2, 2, 1], dtype=np.float32), np.array([4, 2.5, 2, 7, 1], dtype=np.float32), np.array([1, 3.5, 7, 2, 8], dtype=np.float32)]]
-    dt = [[np.array([4, 5.5, 5, 2, 1], dtype=np.float32), np.array([8, 3.5, 15, 20, 1], dtype=np.float32), np.array([3.5, 5.5, 13, 2, 8], dtype=np.float32)]]
+    gt = [[np.array([1, 3.5, 2, 2, 1], dtype=np.float32), np.array([4, 2.5, 2, 7, 1], dtype=np.float32), 
+    np.array([1, 3.5, 7, 2, 8], dtype=np.float32)]]
+    dt = [[np.array([4, 5.5, 5, 2, 1], dtype=np.float32), np.array([8, 3.5, 15, 20, 1], dtype=np.float32),
+    np.array([3.5, 5.5, 13, 2, 8], dtype=np.float32)]]
     print(f'Final metrics = {metric_elps(gt, dt)}')
